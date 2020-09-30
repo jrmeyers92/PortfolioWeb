ca1e1bb7a324eca9bb78778d3d81e052f9beb1ea
Jake Meyers <jakejake92@yahoo.com>
2020-09-30 15:46:32 -0600

    nav done including nav list

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // event listener for hamburger
+
 const hamburger = document.querySelector(".hamburger--stand");
 hamburger.addEventListener("click", () => {
+	const navList = document.querySelector(".nav__list");
 	hamburger.classList.toggle("is-active");
+
+	if (hamburger.classList.contains("is-active")) {
+		navList.classList.add("translate");
+	} else if (!hamburger.classList.contains("is-active")) {
+		navList.classList.remove("translate");
+	}
 });
