ad9bfd0c9e1b5d29175d2caf596e74fb49f9f258
Jake Meyers <jakejake92@yahoo.com>
2020-12-12 14:33:45 -0700

    deployed firebase function

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const config = functions.config();
+const cors = 
+admin.initializeApp();
+
+const transportor = nodemailer.createTransport({
+	service: "gmail",
+	auth: { user: config.user.email, pass: config.user.password },
+});
+
+let mailOptions = {
+	from: "Jake's Web Dev inc.",
+	to: "jakermeyers92@gmail.com",
+	subject: "testing",
+	text: "test successful",
+};
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.sendMail = functions.https.onRequest((request, response) => {
+	transportor.sendMail(mailOptions, (error) => {
+		if (error) {
+			response.send(error);
+		} else {
+			response.send("message succesffully sent");
+		}
+	});
+});
