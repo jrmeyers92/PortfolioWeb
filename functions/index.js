930d045a87830c134133f1ffba654a9bb9b3f2f4
Jake Meyers <jakejake92@yahoo.com>
2020-12-14 18:49:03 -0700

    tinkering with firebase

--- functions/index.js
+++ functions/index.js
@@ -5,29 +5,45 @@ const config = functions.config();
 const cors = require("cors")({ orgin: true });
 admin.initializeApp();
 
-const transportor = nodemailer.createTransport({
+let transportor = nodemailer.createTransport({
 	service: "gmail",
 	auth: { user: config.user.email, pass: config.user.password },
 });
 
-let mailOptions = {
-	from: "Jake's Web Dev inc.",
-	to: "jakermeyers92@gmail.com",
-	subject: "testing",
-	text: "test successful",
-};
+// const email = document.getElementById("email").value;
+// const message = document.getElementById("message").value;
+// const name = document.getElementById("name").value;
+
+// let mailOptions = {
+// 	from: "Jake's Web Dev inc.",
+// 	to: "jakermeyers92@gmail.com",
+// 	subject: "email from form",
+// 	html: `Name: ${name} - Email: ${email} - Message: ${message}`,
+// };
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.sendMail = functions.https.onRequest((request, response) => {
 	cors(request, response, () => {
-		transportor.sendMail(mailOptions, (error) => {
-			if (error) {
-				response.send(error);
-			} else {
-				response.send("message succesffully sent");
+		const dest = request.query.dest;
+
+		const email = document.getElementById("email").value;
+		const message = document.getElementById("message").value;
+		const name = document.getElementById("name").value;
+
+		let mailOptions = {
+			from: "Jake's Web Dev inc.",
+			to: request.query.dest,
+			subject: "email from form",
+			html: "hi",
+		};
+
+		return transportor.sendMail(mailOptions, (err, info) => {
+			if (err) {
+				return response.send(err.toString());
 			}
+			return response.send("sended");
 		});
 	});
 });
