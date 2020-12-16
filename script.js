929aeff096b2a23aa50ab08abce11e1ff44a6926
Jake Meyers <jakejake92@yahoo.com>
2020-12-15 18:03:34 -0700

    added animation to home screen

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//animations
+const timeline = gsap.timeline({ defaults: { duration: 0.8 } });
+timeline
+	.from(".home__h2", { y: "-1000%", ease: "bounce" })
+	.from(".home__h3", { x: "-500%", ease: "back" })
+	.from(".nav__item", { opacity: 0, stagger: 0.5 });
+
+gsap.from(".nav__block", { opacity: 0, duration: 3, delay: 2 });
+
 // event listener for hamburger
 
 const hamburger = document.querySelector(".hamburger--stand");
@@ -33,4 +42,26 @@ document.body.addEventListener("click", (event) => {
 		navList.classList.remove("translate");
 		hamburger.classList.remove("is-active");
 	}
+});
+
+// event listener for form submit
+
+const formBtn = document.getElementById("form-submit");
+formBtn.addEventListener("click", async () => {
+	await fetch(
+		"https://cors-anywhere.herokuapp.com/https://us-central1-jake-meyers-web-dev-portfolio.cloudfunctions.net/sendMail"
+	)
+		.then((res) => console.log(res))
+		.catch((err) => console.log({ err }));
+});
+
+// event listener for modal
+formBtn.addEventListener("click", () => {
+	const modal = document.getElementById("modal");
+	modal.classList.add("show");
+});
+
+const modalExit = document.getElementById("modal__exit");
+modalExit.addEventListener("click", () => {
+	modal.classList.remove("show");
 });
